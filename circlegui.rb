39e583be69894a1bcb0ef7a496b02f19c228d835
Jorge A. Cano <jacinaustin@gmail.com>
2017-01-20 12:55:27 -0600

    a virtual dancing circle
    Shoes tutorial guide... simple gui app

---
+++ circlegui.rb
@@ -0,0 +1,14 @@
+# code for circles in shoes
+
+Shoes.app do
+ @o = oval :top => 0, :left => 0,
+           :radius => 40
+ stack :margin =>40 do
+   title "Dancing With a Circle"
+   subtitle "How graceful and round. You Sping me round baby, baby right round"
+   end
+
+   motion do |x, y|
+     @o.move width - x, height - y
+   end 
+end
